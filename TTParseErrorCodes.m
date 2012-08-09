e0ef83c4eee027bc69550aae1e40d93b7e1c3688
nodemaker <nodemaker@gmail.com>
2012-08-09 15:29:01 -0400

    Small Fix

--- TTParseErrorCodes.m
+++ TTParseErrorCodes.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2011 Sumeru Chatterjee. All rights reserved.
 //
 
-#import "TTErrorCodes.h"
+#import "TTParseErrorCodes.h"
 
 NSString* const kTTExtParseErrorDomain = @"three20.ext.parse";
 NSInteger const kTTExtParseErrorCodeInvalidObject = 100;
